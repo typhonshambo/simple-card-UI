e522a2c566d70c699ad396869f10e6cea4d5421a
Shambo Chowdhury <54593764+typhonshambo@users.noreply.github.com>
2024-02-26 21:24:38 +0530

    update

--- simple-card-UI/ContentView.swift
+++ simple-card-UI/ContentView.swift
@@ -9,12 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing : 20){
+         
+            Image("ooty")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(20)
+            HStack{
+                Spacer()
+                Text("Sample Text")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    
+                    
+                    
+                Spacer()
+                
+                VStack{
+                    
+                    HStack{
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.leadinghalf.filled")
+                    }
+                    
+                    
+                    Text("Rating 4.8")
+                        
+ 
+                }.foregroundColor(.orange)
+                    .font(.caption)
+
+                Spacer()
+       
+                
+            }
+            
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+
+            
+
         }
+        .padding()
+        .background(Rectangle().foregroundColor(.white))
+        .cornerRadius(20)
+        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
         .padding()
     }
 }
