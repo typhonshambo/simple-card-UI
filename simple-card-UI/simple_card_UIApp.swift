852e733de1467a6db0aeea9020971aa2fdd8580b
Shambo Chowdhury <54593764+typhonshambo@users.noreply.github.com>
2024-02-26 20:18:23 +0530

    Initial Commit

---
+++ simple-card-UI/simple_card_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_card_UIApp.swift
+//  simple-card-UI
+//
+//  Created by Shambo Chowdhury on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct simple_card_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
